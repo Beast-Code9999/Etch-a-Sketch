573b3f8f3d57773e7ffe377b123d8c24f77e6bea
Beast Code <beastcode9999@gmail.com>
2022-07-15 20:40:20 +0900

    assign slider to slider.value

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const modeBtn = document.querySelectorAll('.mode button');
 const erase = document.querySelector('.erase')
 const gridContainer = document.querySelector('.grid-container');
 const sliderH1 = document.querySelector('.slider  h1');
-const slider = document.getElementById('slider');
+const slider = document.getElementById('slider').value;
 const checkToggle = document.getElementById('check');
+
+// varible to return value of slider 
+
+
 
