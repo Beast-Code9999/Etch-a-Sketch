243f094c647e654d4b7f592b0bc50f03d3f800b8
Beast Code <beastcode9999@gmail.com>
2022-07-15 19:21:16 +0900

    initial selectors setup

--- script.js
+++ script.js
@@ -3,3 +3,6 @@ const modeBtn = document.querySelectorAll('.mode button');
 const erase = document.querySelector('.erase')
 const gridContainer = document.querySelector('.grid-container');
 const sliderH1 = document.querySelector('.slider  h1');
+const slider = document.getElementById('slider');
+const checkToggle = document.getElementById('check');
+
