7133b6d18e60749133c35a7ce28e0bb4366a52a8
Beast Code <beastcode9999@gmail.com>
2022-07-15 19:14:02 +0900

    select and edit selectors

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 const colorPicker = document.getElementById('colorPicker');
+const modeBtn = document.querySelectorAll('.mode button');
+const erase = document.querySelector('.erase')
+const gridContainer = document.querySelector('.grid-container');
+const sliderH1 = document.querySelector('.slider  h1');
