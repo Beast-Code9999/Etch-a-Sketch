d6ec483f80d8681aaade41fed1750e542665af84
Beast Code <beastcode9999@gmail.com>
2022-07-15 20:51:09 +0900

    add updateH1 function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const colorPicker = document.getElementById('colorPicker');
+const colorPicker = document.getElementById('colorPicker').value;
 const modeBtn = document.querySelectorAll('.mode button');
 const erase = document.querySelector('.erase')
 const gridContainer = document.querySelector('.grid-container');
@@ -6,7 +6,18 @@ const sliderH1 = document.querySelector('.slider  h1');
 const slider = document.getElementById('slider').value;
 const checkToggle = document.getElementById('check');
 
-// varible to return value of slider 
 
+console.log(sliderH1.textContent)
+// create a function to update textContent of sliderH1
+function updateH1(size) {
+    sliderH1.textContent = `${size}x${size}`
+}
+
+
+
+//write a function to create divs
+function createDiv() {
+
+}
 
 
