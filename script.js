df1cf4e5765dc27b5308f9b3c579e56fad31dd5d
Beast <beastcode9999@gmail.com>
2022-07-19 12:47:12 +0900

    add functions to create default settings on load

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const modeBtn = document.querySelectorAll('.mode button');
 const erase = document.querySelector('.erase')
 const gridContainer = document.querySelector('.grid-container');
 const sliderH1 = document.querySelector('.slider  h1');
-const slider = document.getElementById('slider').value;
+const slider = document.querySelector('slider');
+const sliderValue = document.getElementById('slider').value;
 const checkToggle = document.getElementById('check');
 let mode = 'default';
 
@@ -16,24 +17,38 @@ function updateH1(size) {
 
 // create divs inside container
 function createDivs() {
-    gridContainer.setAttribute('style', `display: grid; grid-template-columns: repeat(${slider}, 1fr)`)
-    for(let i = 1; i <= (slider * slider); i++) {
+    gridContainer.setAttribute('style', `display: grid; grid-template-columns: repeat(${sliderValue}, 1fr)`)
+    for(let i = 1; i <= (sliderValue * sliderValue); i++) {
         const div = document.createElement('div');
         gridContainer.appendChild(div);
+        updateH1(sliderValue);
     }
 
 }
 createDivs()
+
+// listen to slider on click and dynamically change the vlaue of divs * sivs and textContent
+
+
+// when windows load, give default settings
+function createTraitsOnLoad() {
+    window.addEventListener('load', () => {
+        mode = 'default'
+        changeColor('mouseenter');
+    })
+}
+
+createTraitsOnLoad();
 
 // selecting mode 
 function getMode() {
     modeBtn.forEach(button => {
         button.addEventListener('click', () => {
             mode = button.textContent.toLowerCase();
-            
+            console.log(mode)
+            changeColor('mouseenter');
         })
     })
-    
 }
 
 //change color of divs inside container on hover
@@ -53,12 +68,19 @@ function changeColor(type) {
     const gridDiv = gridContainer.querySelectorAll('div');
     gridDiv.forEach(div => {
         div.addEventListener(type, e => {
-            e.target.style.backgroundColor = 'gray'
+            if(mode == 'default' || mode == 'classic') {
+                e.target.style.backgroundColor = 'gray'
+            }
+            else if(mode == 'modern') {
+                e.target.style.backgroundColor = 'black'
+            }
         })
     })
 }
 
-changeColor('mouseenter');
+getMode()
+
+
 
 
 
